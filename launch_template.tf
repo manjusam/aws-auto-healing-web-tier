c8730b32a53882e43bebd253733060418a6418da
Manju Sam <manjusam403@gmail.com>
2026-09-18 17:23:06 +1000

    Add EC2 launch template

---
+++ launch_template.tf
@@ -0,0 +1,42 @@
+# -----------------------------
+# Amazon Linux 2023 AMI
+# -----------------------------
+
+data "aws_ssm_parameter" "al2023_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+
+# -----------------------------
+# EC2 Launch Template
+# -----------------------------
+
+resource "aws_launch_template" "web" {
+  name = "auto-healing-web-lt"
+
+  image_id      = data.aws_ssm_parameter.al2023_ami.value
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2.id
+  ]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+
+    dnf update -y
+    dnf install -y nginx
+
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "auto-healing-web"
+    }
+  }
+}
